19d1ca3b2b8e1db150dd508e7236e9fbde14458e
Omrinaf <naftaliomri@gmail.com>
2025-07-01 19:29:19 +0300

    Update Omri_Naftali_recommendations.sql

--- recommendations_goldstandard/recommendations/Omri_Naftali_recommendations.sql
+++ recommendations_goldstandard/recommendations/Omri_Naftali_recommendations.sql
@@ -1,204 +1,3 @@
-use imdb_ijs;
-
-# selecting 100 good recommendations
-# first, I'll recommend movies that someone who likes Oceans Eleven might like
-
-select id
-from movies
-where
-name = "Ocean's Eleven"
-and year = 2001
-;
-
-# Ocean's Elaven's ID is 238072
-# selecting movies with more at least 3 common actors
-
-with target_cast as (
-select actor_id
-from
-roles as r
-where
-r.movie_id = 238072
-)
-select r.movie_id
-from
-roles as r
-join
-target_cast as tg
-on
-r.actor_id = tg.actor_id
-where
-r.movie_id <> 238072
-group by
-r.movie_id
-having 
-count(*) > 2
-;
-
-# we got 21 recommendations: 14132, 238073, 56304, 317437, 335872, 1674, 4642, 107385, 160473, 245103, 337404, 846, 13751,
-# 69391, 299317, 166939, 351187, 2603, 290086, 257522, 121528
-
-# now we suggest movies by the same director
-
-with target_d as (
-select director_id
-from
-movies_directors
-where
-movie_id = 238072
-)
-select md.movie_id
-from
-movies_directors as md
-join
-target_d as td
-where 
-md.director_id = td.director_id
-and
-movie_id <> 238072
-;
-
-# we got 19 recommendations: 60107, 102514, 102653, 121528, 133382, 172073, 177456, 190951, 238073, 245103, 290086, 296024,
-# 307274, 337404, 346299, 366383, 371340, 388254, 394001
-# 40 so far, now we suggest movies with at least 3 shared genres
-
-with target_genres as (
-select genre
-from
-movies_genres
-where 
-movie_id = 238072
-)
-select mg.movie_id
-from
-movies_genres as mg
-join 
-target_genres as tg
-on 
-mg.genre = tg.genre
-where mg.movie_id <> 238072
-group by mg.movie_id
-having
-count(*) > 2
-order by count(*) desc
-limit 30
-;
-
-# we got 30 recommendations: 361, 955, 1369, 2174, 2250, 2597, 2763, 2840, 4294, 5148, 9084, 13224, 17934, 18015, 20882, 
-# 25924, 27168, 27171, 27928, 32080, 36824, 37473, 38553, 48777, 48909, 49306, 49312, 59293, 59829, 60148
-# 70 so far, now we suggest movies that someone who likes Forrest Gump might like
-
-select id
-from
-movies
-where
-name = "Forrest Gump"
-;
-
-# Forrest Gump's ID is 117874
-# selecting movies with at least 3 common actors, using the same logic as earlier
-
-with target_cast as (
-select actor_id
-from
-roles
-where
-movie_id = 117874
-)
-select r.movie_id
-from
-roles as r
-join
-target_cast as tc
-on
-r.actor_id = tc.actor_id
-where
-r.movie_id <> 117874
-group by r.movie_id
-having
-count(*) > 2
-limit 30 # to get 100 recommendations
-;
-
-# we got 30 recommendations: 65501, 389416, 1404, 18979, 69895, 87699, 116950, 203572, 333182, 383666, 385285, 6029, 90932,
-# 233307, 306364, 31944, 212716, 923, 34648, 35629, 105721, 118271, 151093, 167324, 210854, 274620, 359185, 410168, 329522,
-# 105723
-# now we have 100 good recommendations
-
-# now, 100 not good but resonable recommendations
-# we suggest movies that are reasonable for someone who likes Men in Black to like as well
-
-select id
-from
-movies
-where
-name = "Men in Black"
-and year = 1997;
-
-# Men in Black's ID 210739
-# selecting movies with 2 common actors, using the same logic as earlier
-
-with target_cast as (
-select actor_id
-from
-roles
-where
-movie_id = 210739
-)
-select r.movie_id
-from
-roles as r
-join
-target_cast as tg
-on
-r.actor_id = tg.actor_id
-where
-r.movie_id <> 210739
-group by
-r.movie_id
-having 
-count(*) = 2
-;
-
-# we got 49 recommendations: 26442, 184859, 212653, 79875, 123034, 126555, 171278, 400236, 215813, 160492, 1681, 51661, 
-# 67988, 127237, 305379, 385174, 97360, 157240, 167324, 329401, 362716, 192100, 111783, 386966, 19294, 30440, 56748, 83789,
-# 141051, 246260, 386414, 28635, 248132, 276201, 136510, 348303, 50866, 379716, 406621, 6248, 185648, 210740, 82055, 
-# 365642, 35764, 24429, 30967, 170982, 37473
-# now, we suggest movies with 2 shared genres with Men in Black
-
-with target_genres as (
-select genre
-from
-movies_genres
-where
-movie_id = 210739
-)
-select mg.movie_id
-from
-movies_genres as mg
-join
-target_genres as tg
-on
-mg.genre = tg.genre
-where
-mg.movie_id <> 210739
-group by mg.movie_id
-having
-count(*) = 2
-limit 51
-;
-
-# we got 51 recommendations: 50, 52, 53, 94, 322, 328, 344, 382, 465, 510, 516, 517, 527, 881, 892, 1018, 1294, 1432, 1490,
-# 1581, 1642, 1710, 1712, 1715, 1750, 1756, 1799, 1921, 2045, 2134, 2136, 2178, 2182, 2206, 2250, 2414, 2511, 2537, 2597, 
-# 2730, 2763, 2964, 2989, 2995, 2999, 3097, 3109, 3241, 3315, 3421, 3983
-# now we got 100 not good but reasonable recommendations
-
-# recommendation rating:
-# movies with at least 3 shared actors will be rated as 8
-# movies with the same director will be rated as 9
-# movies with at least 3 shared genres will be rated as 8
-# movies with 2 shared actors will be rated 4
-# movies with 2 shared genres will be rated 4
 
 # inserting the recommendations into the movies_recommendations table
 
@@ -330,112 +129,112 @@ values
 
 insert into imdb_ijs.movies_recommendations
 values
-(210739, 26442, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 184859, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 212653, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 79875, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 123034, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 126555, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 171278, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 400236, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 215813, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 160492, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 1681, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 51661, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 67988, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 127237, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 305379, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 385174, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 97360, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 157240, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 167324, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 329401, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 362716, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 192100, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 111783, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 386966, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 19294, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 30440, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 56748, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 83789, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 141051, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 246260, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 386414, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 28635, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 248132, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 276201, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 136510, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 348303, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 50866, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 379716, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 406621, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 6248, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 185648, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 210740, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 82055, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 365642, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 35764, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 24429, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 30967, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 170982, 4, "Omri Naftali", "2 shared actors", NULL),
-(210739, 37473, 4, "Omri Naftali", "2 shared actors", NULL)
+(210739, 26442, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 184859, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 212653, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 79875, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 123034, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 126555, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 171278, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 400236, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 215813, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 160492, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 1681, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 51661, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 67988, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 127237, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 305379, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 385174, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 97360, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 157240, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 167324, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 329401, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 362716, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 192100, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 111783, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 386966, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 19294, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 30440, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 56748, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 83789, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 141051, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 246260, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 386414, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 28635, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 248132, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 276201, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 136510, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 348303, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 50866, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 379716, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 406621, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 6248, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 185648, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 210740, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 82055, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 365642, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 35764, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 24429, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 30967, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 170982, 4, "Omri Naftali", "only 2 shared actors", NULL),
+(210739, 37473, 4, "Omri Naftali", "only 2 shared actors", NULL)
 ;
 
 # Men in Black - 2 shared genres
 
 insert into imdb_ijs.movies_recommendations
 values
-(210739, 50, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 52, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 53, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 94, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 322, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 328, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 344, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 382, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 465, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 510, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 516, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 517, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 527, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 881, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 892, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 1018, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 1294, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 1432, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 1490, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 1581, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 1642, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 1710, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 1712, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 1715, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 1750, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 1756, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 1799, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 1921, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 2045, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 2134, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 2136, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 2178, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 2182, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 2206, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 2250, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 2414, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 2511, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 2537, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 2597, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 2730, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 2763, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 2964, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 2989, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 2995, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 2999, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 3097, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 3109, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 3241, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 3315, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 3421, 4, "Omri Naftali", "2 shared genres", NULL),
-(210739, 3983, 4, "Omri Naftali", "2 shared genres", NULL)
+(210739, 50, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 52, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 53, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 94, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 322, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 328, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 344, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 382, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 465, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 510, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 516, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 517, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 527, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 881, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 892, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 1018, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 1294, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 1432, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 1490, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 1581, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 1642, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 1710, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 1712, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 1715, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 1750, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 1756, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 1799, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 1921, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 2045, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 2134, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 2136, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 2178, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 2182, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 2206, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 2250, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 2414, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 2511, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 2537, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 2597, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 2730, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 2763, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 2964, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 2989, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 2995, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 2999, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 3097, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 3109, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 3241, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 3315, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 3421, 4, "Omri Naftali", "only 2 shared genres", NULL),
+(210739, 3983, 4, "Omri Naftali", "only 2 shared genres", NULL)
 ;
 
 
