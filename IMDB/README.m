72f5a245b99c153ca87eb65eedb9535ad822968a
evidencebp <idanamit@gmail.com>
2025-08-04 09:38:29 +0300

    Create README.m

---
+++ IMDB/README.m
@@ -0,0 +1,4 @@
+This directory contains the materials needed to create the IMDB schema, used a lot in this course.
+The data directory has a zipped schema export (see assignments for installation details).
+Schema_Structure has three IMDB just table creation exports (for representation comparison).
+We also have a png of the imdb_ijs schema, to ease use.
